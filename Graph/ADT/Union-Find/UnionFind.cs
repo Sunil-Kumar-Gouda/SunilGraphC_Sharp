92a1807ad73a7dd9105675443c1908d990d09cda
Sunil-Kumar-Gouda <sunilkumargouda.o@outlook.com>
2017-09-28 16:47:01 +0530

    UnionFindPartial

--- Graph/ADT/Union-Find/UnionFind.cs
+++ Graph/ADT/Union-Find/UnionFind.cs
@@ -6,13 +6,46 @@ using System.Threading.Tasks;
 
 namespace ADT.Union_Find
 {
-    class UnionFind<T> where T : class
+    class UnionFind<T> where T : struct
     {
-        public Dictionary<T, int> _id;
         private int _count;
-        public UnionFind()
-        {
-            _id = new Dictionary<T, int>();
-        }
+        private int  _id;
+
+
+
+
+
+        //public Dictionary<T, int> _id;
+        //public Dictionary<T, int> _size;
+        //public int Count
+        //{
+        //    get { return _count; }
+        //    set { _count = value; }
+        //}
+        //public int this[T key]
+        //{
+        //    get { return _id[key]; }
+        //    private set
+        //    {
+        //        _id[key] = value;
+        //    }
+        //}
+        //public UnionFind()
+        //{
+        //    _id = new Dictionary<T, int>();
+        //}
+        //public bool AddData(T data)
+        //{
+        //    if (!_id.ContainsKey(data))
+        //    {
+        //        _id.Add(data, Count++);
+        //        return true;
+        //    }
+        //    return false;
+        //}
+        //public int Find(T data)
+        //{
+        //    return this[data];
+        //}
     }
 }
