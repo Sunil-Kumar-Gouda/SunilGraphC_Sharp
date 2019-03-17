ae929e737297df49fde1a5d6a037a15bd6c13928
Sunil-Kumar-Gouda <sunilkumargouda.o@outlook.com>
2019-03-18 01:14:30 +0800

    algo

---
+++ Graph/AlgoExpert/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoExpert.ArrayT;
+
+namespace AlgoExpert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var answer= NumberSum.ThreeNumberSum(new int[] { 12, 3, 1, 2, -6, 5, -8, 6 }, 0);
+        }
+    }
+}
