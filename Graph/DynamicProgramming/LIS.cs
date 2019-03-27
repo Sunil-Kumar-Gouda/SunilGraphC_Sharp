27e53d3d5917ea43c3cdf27db1e47062af2c58ad
Sunil-Kumar-Gouda <sunilkumargouda.o@outlook.com>
2019-03-27 08:24:01 +0800

    algoexpert

---
+++ Graph/DynamicProgramming/LIS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    public static class LIS
+    {
+        public static int LongestIncreasingSubsequence(int[] ar)
+        {
+            int[] lis = Enumerable.Repeat(1,ar.Length).ToArray();
+            for (int i = 1; i < ar.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (ar[i] > ar[j] && (lis[j] + 1) > lis[i])
+                        lis[i] = lis[j] + 1;
+                }
+            }
+            int max = -1;
+            for (int j = 0; j < lis.Length; j++)
+            {
+                if (lis[j] > max)
+                    max = lis[j];
+            }
+            return max;
+        }
+    }
+}
---
+++ Graph/ProblemSolving/TreeProblem/TreeProblem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.TreeProblem
+{
+    static public class TreeProblem
+    {
+        public static int CountLeafAfterRemove(int []ar,int removeNode)
+        {
+            int []notLeafNode = new int[ar.Length];
+            for(int i=1;i<ar.Length;i++)
+            {
+                if (ar[ar[i]] == int.MinValue)
+                {
+                    ar[i] = int.MinValue;
+                }
+                if (ar[i] == removeNode || i == removeNode)
+                {
+                    notLeafNode[i] = 1;
+                }
+                else
+                {
+                    notLeafNode[ar[i]] = 1;
+                }
+            }
+
+            return notLeafNode.Count(x => x != 1);
+        }
+    }
+}
